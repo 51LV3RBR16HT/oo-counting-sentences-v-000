6150b6d21d08f703a825b5eef4d47c93c0099177
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-15 18:02:37 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,8 +17,8 @@ class String
   def count_sentences
     complex_string = self.split(/[!.]/)
     complex_string.length
-    # The trick was to use regex to identify puctuation in order to count
-    # each sentence: http://rubular.com/
+    # The trick was to use regex after you split your string to identify
+    # the puctuation in order to count each sentence: http://rubular.com/
   end
 
 end
