2e0c9cca326cc12dd033a7d90297fd01bb6ff27c
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-15 18:03:40 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,7 +18,7 @@ class String
     complex_string = self.split(/[!.]/)
     complex_string.length
     # The trick was to use regex after you split your string to identify
-    # the puctuation in order to count each sentence: http://rubular.com/
+    # and use punctuation in order to count each sentence: http://rubular.com/
   end
 
 end
